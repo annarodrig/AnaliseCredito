73b592c773b57a294e07a0b692c052050df0dc06
Anna Beatriz <annab_oliver@hotmail.com>
2022-06-14 22:12:06 -0300

    Inicia exercicio

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int  parcelas;
+decimal valorSolicitado,  rendaMensal, valorParcela;
+
+Console.WriteLine("-- Solicitação de empréstimo --");
+
+Console.Write("\nInforme o valor solicitado:");
+valorSolicitado = Convert.ToDecimal(Console.ReadLine()!);
+
+Console.Write("\nInforme a quantidade de parcelas que deseja:");
+parcelas = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("\nInforme o valor de sua renda mensal comprovada:");
+rendaMensal = Convert.ToDecimal(Console.ReadLine()!);
+
+valorParcela = valorSolicitado / parcelas;
+
+
+if ()
+{
+
+}
+
+void EmprestimoAprovado()
+{
+    valorParcela 
+}
+
+
