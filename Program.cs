1ab322d4def94b7776b1d09a2d09882dae10c976
Anna Beatriz <annab_oliver@hotmail.com>
2022-06-18 19:35:53 -0300

    Finaliza o exercício

--- Program.cs
+++ Program.cs
@@ -1,18 +1,34 @@
 ﻿int  parcelas;
-decimal valorSolicitado,  rendaMensal, valorParcela;
+double valorSolicitado,  rendaMensal, valorParcela, calculo;
 
 Console.WriteLine("-- Solicitação de empréstimo --");
 
 Console.Write("\nInforme o valor solicitado:");
-valorSolicitado = Convert.ToDecimal(Console.ReadLine()!);
+valorSolicitado = Convert.ToDouble(Console.ReadLine()!);
 
 Console.Write("\nInforme a quantidade de parcelas que deseja:");
 parcelas = Convert.ToInt32(Console.ReadLine()!);
 
 Console.Write("\nInforme o valor de sua renda mensal comprovada:");
-rendaMensal = Convert.ToDecimal(Console.ReadLine()!);
+rendaMensal = Convert.ToDouble(Console.ReadLine()!);
 
 valorParcela = valorSolicitado / parcelas;
 
+Console.WriteLine($"\nVocê solicitou o valor de R${valorSolicitado} em {parcelas}X de R${valorParcela}\n");
 
+calculo = rendaMensal * 0.3;
 
+if (valorParcela > calculo)
+{
+    Console.ForegroundColor=ConsoleColor.Red;
+    Console.WriteLine($"--- Seu empréstimo foi negado! ---");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor=ConsoleColor.Green;
+    Console.WriteLine("--- Seu empréstimo foi aprovado! ---");
+    Console.ResetColor();
+}
+
+Console.WriteLine("\nObrigado por utilizar nossos serviços!");
